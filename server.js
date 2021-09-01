f26714857c44099572b5d50e7b631cb6e0afd84d
UmarAli <umaralimuminjonov@gmail.com>
2021-09-01 19:20:52 +0500

    update

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.use("/api", require("./routes/workRouter"));
 app.use("/api", require("./routes/upload"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
