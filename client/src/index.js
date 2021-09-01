e144e7a50635784ee2070f81a828ff4c349afa65
UmarAli <umaralimuminjonov@gmail.com>
2021-09-01 20:20:11 +0500

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,7 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "./i18n";
 
-ReactDOM.render(
-  <Suspense fallback="Loading...">
-    <App />
-  </Suspense>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
